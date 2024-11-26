00ebe4bfc12e0ba3c45dffb8941920565121ec9c
SumanAdhithya30 <143721627+SumanAdhithya30@users.noreply.github.com>
2024-11-26 23:07:48 +0530

    Add files via upload

---
+++ ContinuousPyramid.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class ContinuousPyramid {
+    public static void main(String[] args) {
+      int n=5,num=1;
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=i;j++){
+              System.out.print(num+" ");
+              num++;
+          }
+          System.out.println();
+      }
+    }
+}
